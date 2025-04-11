03b088c885c4bb1af7fc73174781b20690f758c2
Susanadjanie1 <susanadjanie1@gmail.com>
2025-04-11 07:03:11 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-bold flex justify-center text-blue-300 items-center text-9xl'>DocOnGo</h1>
+      <p className='flex items-center justify-center p-3 font-semibold italic '>Your Pocket Doctor</p>
+    </>
+  )
+}
+
+export default App
