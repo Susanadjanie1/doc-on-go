9017375a2401acea15d556ced124ca0b65cd08c2
Susanadjanie1 <susanadjanie1@gmail.com>
2025-04-14 09:41:47 +0000

    login error

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
             <input
               type="email"
               name="email"
-              placeholder="jane@example.com"
+              placeholder="jane@gmail.com"
               required
               className="mt-1 w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#F97171]"
             />
